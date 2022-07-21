ded45533141a7e471dc033ffa35d5dc92810a52d
Jonathon Wazney <jonathonwazney@Jonathons-Air.lan>
2022-07-21 15:13:33 -0500

    database

---
+++ src/db/migration.sql
@@ -0,0 +1,53 @@
+DROP DATABASE IF EXISTS techPort_db;
+
+CREATE DATABASE techPort_db;
+\l
+\c techPort_db;
+ 
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS tickets CASCADE;
+DROP TABLE IF EXISTS universities CASCADE;
+
+CREATE TABLE universities(
+    university_id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR,
+    logo_url TEXT,
+    background_url TEXT
+);
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(64),
+    password TEXT,
+    email VARCHAR,
+    role INT,
+    university_id INT REFERENCES universities(university_id) 
+);
+
+CREATE TABLE tickets(
+    ticket_id SERIAL PRIMARY KEY NOT NULL,
+    open_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP(0),
+    close_date TIMESTAMP,
+    problem VARCHAR(64),
+    description TEXT,
+    note TEXT,
+    point_of_contact VARCHAR(64),
+    location TEXT,
+    priority INT,
+    status INT,
+    university_id INT REFERENCES universities(university_id),
+    assigned_tech INT REFERENCES users(user_id)
+);
+
+
+\i src/db/seed.sql
+
+SELECT * FROM users;
+SELECT * FROM tickets;
+SELECT * FROM universities;
+
+\d
+\dt 
+\d users
+\d tickets
+\d universities
