9121d835c96206b9625f52b4dd0a1b161bc4a6d3
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-07-22 10:11:34 -0700

    Changes the pool creation to integrate a method to check production or deployment state and add SSL routing

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,8 +1,13 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
+const pool = new Pool(poolConfig);
+
+const poolConfig = {
     connectionString: process.env.DATABASE_URL,
-    // ssl: { rejectUnauthorized: false }
-})
+}
+
+if (process.env.NODE_ENV === "production") {
+    poolConfig.ssl = {rejectUnauthorized: false}
+}
 
 module.exports = pool
