aff466f7c7afd135c61d9310882ca15d2134aa48
Shatawni <shatawni.johnson@gmail.com>
2022-07-28 12:29:16 -0400

    Working on the ticket board component.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import LogIn from "./login-page/Login";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "./login-page/SignUp";
 import Logo from "./login-page/Logo";
 import SignIn from "./login-page/SignIn";
 import AdminLandingPage from "./adminLandingPage/adminLandingPage";
+import TicketBoard from "./ticketBoard/ticketBoard";
 
 function App() {
   const [modalIsOpen, setmodalIsOpen] = useState(true);
   return (
-   <Router>
-    <Routes>
-      <Route exact path='/' element={
-        <>
-        <LogIn></LogIn>
-        </>
-      }></Route>
-      <Route exact path='/signup' element={
-        <>
-        <LogIn></LogIn>
-        <SignUp modalIsOpen={modalIsOpen} setmodalIsOpen={setmodalIsOpen}></SignUp>
-        </>
-       
-      }></Route>
-      <Route exact path='/signin' element={
-        <>
-        <SignIn modalIsOpen={modalIsOpen} setmodalIsOpen={setmodalIsOpen}></SignIn>
-        </>
-      }></Route>
-      <Route exact path="/admin" element={
-        <AdminLandingPage/>
-      }></Route>
-    </Routes>
-   </Router>
-   
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <LogIn></LogIn>
+            </>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/signup"
+          element={
+            <>
+              <LogIn></LogIn>
+              <SignUp
+                modalIsOpen={modalIsOpen}
+                setmodalIsOpen={setmodalIsOpen}
+              ></SignUp>
+            </>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/signin"
+          element={
+            <>
+              <SignIn
+                modalIsOpen={modalIsOpen}
+                setmodalIsOpen={setmodalIsOpen}
+              ></SignIn>
+            </>
+          }
+        ></Route>
+        <Route exact path="/admin" element={<AdminLandingPage />}></Route>
+        <Route exact path="/ticketBoard" element={<TicketBoard />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
