611faf1a6eea473f11ca8777c0f6f73783a6fbbb
treyodell8 <treyodell8@gmail.com>
2022-07-28 18:06:58 -0500

    changed connection

--- db/connection.js
+++ db/connection.js
@@ -2,12 +2,13 @@ const { Pool } = require("pg");
 
 const poolConfig = {
     connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false}
 }
 
 const pool = new Pool(poolConfig);
 
-if (process.env.NODE_ENV === "production") {
-    poolConfig.ssl = {rejectUnauthorized: false}
-}
+// if (process.env.NODE_ENV === "production") {
+//     poolConfig.ssl = {rejectUnauthorized: false}
+// }
 
 module.exports = pool
