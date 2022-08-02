0b3781e512029d2d570c6b821e1ea2f1571ccda9
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-02 12:39:25 -0700

    Creates logic for header component

--- src/ticketBoard/header.js
+++ src/ticketBoard/header.js
@@ -1,6 +1,24 @@
 import React from 'react'
 import "./ticketBoard.css";
 
-export default function header() {
+export default function Header() {
+    return (
+        <>
+          <nav className="nav1">
+            <div className="leftPosition">
+              <a className="tech">Tech</a>
+            </div>
+            <div className="centerPosition">
+            <h2 className="myCampus">Campus</h2>
+            </div>
+            <div className="rightPosition">
+              <div className="columnPosition">
+                <a className="myName">Welcome Name</a>
+                <a className="logout" href="/">Logout</a>
+              </div>
+            </div>
+            </nav>
+            </>
+            );
 
 }
