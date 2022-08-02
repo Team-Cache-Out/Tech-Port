4aa89874487fb4e2b92df5338ac8c66a6731c9a4
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-02 12:39:52 -0700

    Creates logic for navbar component

--- src/ticketBoard/navbar.js
+++ src/ticketBoard/navbar.js
@@ -1,6 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./ticketBoard.css";
 
-export default function navbar() {
+export default function Navbar() {
 
+    const [user, setUser] = useState('null')
+
+    if(user.role === 'admin') {
+        return (
+            <nav className="nav2">
+            <div className="nav2Center">
+            <a className="navLinks" href="/newticket">
+              Create Ticket
+            </a>  
+            <a className="navLinks" href="/ticketboard">
+              Ticket Board
+            </a>       
+            <a className="navLinks" href="/campusTechs">
+            Campus Techs
+            </a>  
+            </div>
+             <a className="navLinks" href="/campusReport">
+              Campus Report
+            </a> 
+          </nav>
+        )
+    } else {
+        return (
+            <nav className="nav2">
+            <div className="nav2Center">
+            <a className="navLinks" href="/newticket">
+              Create Ticket
+            </a>  
+            <a className="navLinks" href="/ticketboard">
+              Ticket Board
+            </a>
+            <a className="navLinks" href="/myticket">
+              My Tickets
+            </a>          
+            </div>
+          </nav>
+        )
+    }
+    
 }
