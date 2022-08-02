8e12e78edbb5369f4d00fffff8efb90f316ab2e1
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-02 15:02:44 -0700

    Changes syntax to use context

--- src/ticketBoard/header.js
+++ src/ticketBoard/header.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./ticketBoard.css";
+import SignInContext from '../login-page/SignInContext';
 
 export default function Header() {
+
+  const { user } = useContext(SignInContext)
+
     return (
         <>
           <nav className="nav1">
             <div className="leftPosition">
-              <a className="tech">Tech</a>
+              <a className="role">tech</a>
             </div>
             <div className="centerPosition">
             <h2 className="myCampus">Campus</h2>
             </div>
             <div className="rightPosition">
               <div className="columnPosition">
-                <a className="myName">Welcome Name</a>
+                <a className="myName">Welcome name</a>
                 <a className="logout" href="/">Logout</a>
               </div>
             </div>
             </nav>
-            </>
-            );
+          </>
+    );
 
 }
