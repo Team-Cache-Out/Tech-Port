473ee6b4c6a3b0dcd0815fc80132eff0354569e9
Jonathon Wazney <jonathonwazney@Jonathons-Air.lan>
2022-08-02 15:28:28 -0500

    adminpage working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const path = require('path')
 /* This is requiring the connection.js file in the backend folder. */
 const pool = require("./db/connection");
 
@@ -47,7 +47,22 @@ app.get("/users/:id", async (req,res) => {
         let client = await pool.connect();
 
         const data = await client.query("SELECT * FROM users WHERE user_id=$1", [req.params.id]);
-        res.json(data.rows[0]);
+        res.json(data.rows);
+
+        /* Releasing the client from the database. */
+        client.release();
+    } catch (error) {
+        console.error(error)
+    }
+});
+
+app.get("/techs/:id", async (req,res) => {
+    try {
+         /* Connecting to the database. */
+        let client = await pool.connect();
+
+        const data = await client.query("SELECT * FROM users WHERE university_id=$1 AND role=$2", [req.params.id,'tech']);
+        res.json(data.rows);
 
         /* Releasing the client from the database. */
         client.release();
@@ -162,6 +177,21 @@ app.get("/tickets/:id", async (req,res) => {
 
         const data = await client.query("SELECT * FROM tickets WHERE ticket_id=$1", [req.params.id]);
         res.json(data.rows[0]);
+
+        /* Releasing the client from the database. */
+        client.release();
+    } catch (error) {
+        console.error(error)
+    }
+});
+
+app.get("/university/tickets/:id", async (req,res) => {
+    try {
+         /* Connecting to the database. */
+        let client = await pool.connect();
+
+        const data = await client.query("SELECT * FROM tickets WHERE university_id=$1 AND status=$2", [req.params.id,'open']);
+        res.json(data.rows);
 
         /* Releasing the client from the database. */
         client.release();
@@ -301,9 +331,34 @@ app.get("/university/:id", async (req,res) => {
     try {
          /* Connecting to the database. */
         let client = await pool.connect();
-
-        const data = await client.query("SELECT * FROM universities WHERE university_id=$1", [req.params.id]);
-        res.json(data.rows[0]);
+        let id = req.params.id
+let university = await client.query(`
+    SELECT *
+    FROM universities
+    WHERE university_id = $1
+    `, [id]
+)
+let tickets = await client.query(`
+    SELECT COUNT (*) 
+    FROM tickets
+    WHERE university_id = $1
+    AND status = 'open'
+    `, [id]
+)
+let techs = await client.query(`
+    SELECT COUNT (*) 
+    FROM users
+    WHERE university_id = $1
+    AND role = 'tech'
+    `, [id]
+)
+let uni = {
+    name: university.rows[0].name,
+    logo: university.rows[0].logo_url,
+    tickets: tickets.rows[0].count,
+    techs: techs.rows[0].count,
+}
+        res.json(uni)
 
         /* Releasing the client from the database. */
         client.release();
@@ -331,5 +386,9 @@ app.get("/campus/:id", async (req,res) => {
         console.error(error)
     }
 })
+
+app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "/build/index.html"));
+});
 
 //!--------------------------------------------------------------------------------------------------------
