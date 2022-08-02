82f3e338303e4f22c2c640c3858ccf3400c3a005
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-02 12:40:07 -0700

    Creates logic for single ticket component

--- src/ticketBoard/ticket.js
+++ src/ticketBoard/ticket.js
@@ -1,6 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./ticketBoard.css";
 
-export default function ticket() {
+export default function ticket(prop) {
+    const [singleTicket, setSingleTicket] = useState(null)
+    const [ticketModal, setTicketModal] = useState(false)
 
+    const priorityCircle = document.querySelector('.priorityCircle')
+    
+    const handleClick = (e) => {
+        fetch(`https://worldwide-technical-foundation.herokuapp.com/tickets/${e.target.id}`)
+        .then(response => response.json())
+        .then(data => setSingleTicket(data))
+        if(singleTicket !== null) {
+            setTicketModal(true)
+        }
+    }
+
+    if(prop.element.priority === 'routine') {
+        priorityCircle.className="priorityCircleGreen"
+    }
+    if(prop.element.priority === 'urgent') {
+        priorityCircle.className='priorityCircleYellow'
+    } else {
+        priorityCircle.classname='priorityCircleRed'
+    }
+
+    return (
+        <div id={prop.elem.ticket_id} onClick={handleClick} className="ticket">
+                <div className="innerTicketContainerLeft">
+                  <div className='priorityCircle'>.</div>
+                </div>
+                <div className="innerTicketContainer">
+                  <div className="subject">Subject: its broke</div>                
+                  <div className="location">Location: my desk</div>
+                  <div className="contact">Contact: me</div>
+                </div>
+              </div>
+    )
 }
