698094ca3077fd96d9535965e7986cbf12f0f0b2
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-03 13:00:05 -0700

    Updates the psuedocode

--- src/Context/CampusContext.js
+++ src/Context/CampusContext.js
@@ -4,29 +4,33 @@ const CampusContext = createContext()
 
 export const CampusProvider = ({children}) => {
     
-    // University of Houston
+    // University of Houston [unversity_id = 1]
     const [HoustonOpenTickets, setHoustonOpenTickets] = useState(null)
     const [HoustonWorkingTickets, setHoustonWorkingTickets] = useState(null)
     const [HoustonCompleteTickets, setHoustonCompleteTickets] = useState(null)
 
-    // University of Arizona
+    // University of Arizona [unversity_id = 2]
     const [ArizonaOpenTickets, setArizonaOpenTickets] = useState(null)
     const [ArizonaWorkingTickets, setArizonaWorkingTickets] = useState(null)
     const [ArizonaCompleteTickets, setArizonaCompleteTickets] = useState(null)
 
-    // Oregon State
+    // Oregon State [unversity_id = 3]
     const [OregonOpenTickets, setOregonOpentTickets] = useState(null)
     const [OregonWorkingTickets, setOregonWorkingTickets] = useState(null)
     const [OregonCompleteTickets, setOregonCompleteTickets] = useState(null)
 
-    // Pepperdine University
+    // Pepperdine University [unversity_id = 4]
     const [PepperdineOpenTickets, setPepperdineOpenTickets] = useState(null)
     const [PepperdineWorkingTickets, setPepperdineWorkingTickets] = useState(null)
     const [PepperdineCompleteTickets, setPepperCompleteTickets] = useState(null)
 
-
+    // Ticket modal State
     const [ticketModal, setTicketModal] = useState(false)
     const [singleTicket, setSingleTicket] = useState(null)
+
+    // Current Navbar Component State
+    const [activeComp, setActiveComp] = useState('ticketBoard')
+
 
     // functions
 
@@ -58,7 +62,9 @@ export const CampusProvider = ({children}) => {
         ticketModal,
         setTicketModal,
         singleTicket,
-        setSingleTicket
+        setSingleTicket,
+        activeComp,
+        setActiveComp
     }}>
         {children}
     </CampusContext.Provider>
