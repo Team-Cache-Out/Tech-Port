4ace4f192e48e2d17cbfe58a8f4d03218612ff08
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-04 07:10:55 -0700

    Adds in logic to control rendering based off of current university

--- src/ticketBoard/ticketBoard.js
+++ src/ticketBoard/ticketBoard.js
@@ -4,7 +4,7 @@ import Header from './header'
 import Navbar from './navbar'
 import { useContext } from "react"
 import CampusContext from "../Context/CampusContext";
-// import Ticket from "./ticket";
+import Ticket from "./ticket";
 import TicketCreatePage from '../ticketCreatePage/ticketCreate'
 import SignInContext from "../login-page/SignInContext";
 import SingleTicketModal from "./singleTicketModal";
@@ -12,89 +12,101 @@ import SingleTicketModal from "./singleTicketModal";
 export default function TicketBoard() {
   const { currentUni } = useContext(SignInContext)
 
-  const { HoustonOpenTickets, HoustonWorkingTickets, HoustonCompleteTickets } =
-    useContext(CampusContext);
-  const { ArizonaOpenTickets, ArizonaWorkingTickets, ArizonaCompleteTickets } =
-    useContext(CampusContext);
-  const { OregonOpenTickets, OregonWorkingTickets, OregonCompleteTickets } =
-    useContext(CampusContext);
-  const {
-    PepperdineOpenTickets,
-    PepperdineWorkingTickets,
-    PepperdineCompleteTickets,
-  } = useContext(CampusContext);
+  const { HoustonOpenTickets, HoustonWorkingTickets, HoustonCompleteTickets, ArizonaOpenTickets, ArizonaWorkingTickets, ArizonaCompleteTickets, OregonOpenTickets, OregonWorkingTickets, OregonCompleteTickets, PepperdineOpenTickets,
+  PepperdineWorkingTickets, PepperdineCompleteTickets } = useContext(CampusContext);
 
   const { activeComp, ticketModal } = useContext(CampusContext)
 
+  const campus = [[HoustonOpenTickets, HoustonWorkingTickets, HoustonCompleteTickets],[ArizonaOpenTickets, ArizonaWorkingTickets, ArizonaCompleteTickets],[OregonOpenTickets, OregonWorkingTickets, OregonCompleteTickets],[PepperdineOpenTickets, PepperdineWorkingTickets, PepperdineCompleteTickets]]
+
+  const currentCampus = (currentUni) => {
+    if(currentUni === 1) {
+      return campus[0];
+    }
+    if(currentUni === 2) {
+      return campus[1];
+    }
+    if(currentUni === 3) {
+      return campus[2];
+    }
+    if(currentUni === 4) {
+      return campus[3]
+    }
+  }
 
 // <TicketCreatePage />
-if(activeComp === 'createTicket') {
-    return (
-        <div>
-        <Header />
-        <Navbar />
-        <TicketCreatePage />
-        </div>
-    )
-} else if(activeComp === 'campusTechs') {
-    return (
-        <div>
-        <Header />
-        <Navbar />
-        <h1>CampusTechs</h1>
-        </div>
-    )
-} else if(activeComp === 'campusReport') {
-    return (
-        <div>
-        <Header />
-        <Navbar />
-        <h1>CampusReport</h1>
-        </div>
-    )
-} else if(activeComp === 'myTickets') {
-    return (
-        <div>
-        <Header />
-        <Navbar />
-        <h1>MyTickets</h1>
-        </div>
-    )
-} else {
-    return (
-        <div>
+  if(activeComp === 'createTicket') {
+      return (
+          <div>
           <Header />
           <Navbar />
-  
-          <div className="ticketBoardContainer">
-            <h1 className="ticketBoard-header">Ticket Board</h1>
-            <div className="boardRowPosition">
-              <div className="boardColumnHeader">
-                <h2 className="boardColumnH2">Open</h2>
-              </div>
-              <div className="boardColumnHeader">
-                <h2 className="boardColumnH2">Working</h2>
-              </div>
-              <div className="boardColumnHeader">
-                <h2 className="boardColumnH2">Completed</h2>
-              </div>
-            </div>
-            <div className="boardColumnContainer">
-              <div className="boardColumn">
-              {/* <Ticket />*/}
-              <div>Hello</div>
-              </div>
-              <div className="boardColumn">
-              {/* <Ticket />*/}
-              </div>
-              <div className="boardColumn">
-              {/* <Ticket />*/}
-              </div>
-            </div>
+          <TicketCreatePage />
           </div>
-          <SingleTicketModal show={ticketModal} />
-        </div>
-    );
+      )
+  } else if(activeComp === 'campusTechs') {
+      return (
+          <div>
+          <Header />
+          <Navbar />
+          <h1>CampusTechs</h1>
+          </div>
+      )
+  } else if(activeComp === 'campusReport') {
+      return (
+          <div>
+          <Header />
+          <Navbar />
+          <h1>CampusReport</h1>
+          </div>
+      )
+  } else if(activeComp === 'myTickets') {
+      return (
+          <div>
+          <Header />
+          <Navbar />
+          <h1>MyTickets</h1>
+          </div>
+      )
+  } else {
+      return (
+          <div>
+            <Header />
+            <Navbar />
+    
+            <div className="ticketBoardContainer">
+              <h1 className="ticketBoard-header">Ticket Board</h1>
+              <div className="boardRowPosition">
+                <div className="boardColumnHeader">
+                  <h2 className="boardColumnH2">Open</h2>
+                </div>
+                <div className="boardColumnHeader">
+                  <h2 className="boardColumnH2">Working</h2>
+                </div>
+                <div className="boardColumnHeader">
+                  <h2 className="boardColumnH2">Completed</h2>
+                </div>
+              </div>
+              <div className="boardColumnContainer">
+                <div className="boardColumn">
+                {currentCampus[0].map((elem) => {
+                  return (
+                    <Ticket elem={elem} key={elem.ticket_id} />
+                  ) 
+                })}
+                {/* <Ticket />*/}
+                <div>Hello</div>
+                </div>
+                <div className="boardColumn">
+                {/* <Ticket />*/}
+                </div>
+                <div className="boardColumn">
+                {/* <Ticket />*/}
+                </div>
+              </div>
+            </div>
+            <SingleTicketModal show={ticketModal} />
+          </div>
+      );
   }
 }
 
