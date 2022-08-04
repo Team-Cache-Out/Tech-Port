de8bd9a391639f3c9d06fee2af02e1fec521232f
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-04 09:02:40 -0700

    Changes the logic to conditionally render ticket with the input elem and renders color off of priority

--- src/ticketBoard/ticket.js
+++ src/ticketBoard/ticket.js
@@ -5,8 +5,6 @@ import CampusContext from '../Context/CampusContext'
 export default function Ticket(prop) {
 
     const { setTicketModal, singleTicket, setSingleTicket } = useContext(CampusContext)
-
-    const priorityCircle = document.querySelector('.priorityCircle')
     
     const handleClick = (e) => {
         fetch(`https://worldwide-technical-foundation.herokuapp.com/tickets/${e.target.id}`)
@@ -17,24 +15,26 @@ export default function Ticket(prop) {
         }
     }
 
-    if(prop.element.priority === 'routine') {
-        priorityCircle.className='priorityCircleGreen'
-    }
-    if(prop.element.priority === 'urgent') {
-        priorityCircle.className='priorityCircleYellow'
-    } else {
-        priorityCircle.classname='priorityCircleRed'
+    const priority = () => {
+
+        if(prop.elem.priority === 'Routine') {
+            return 'priorityCircleGreen'
+        } else if(prop.elem.priority === 'Urgent') {
+            return 'priorityCircleYellow'
+        } else {
+            return 'priorityCircleRed'
+        }
     }
 
     return (
         <div id={prop.elem.ticket_id} onClick={handleClick} className="ticket">
-                <div className="innerTicketContainerLeft">
-                  <div className='priorityCircle'>.</div>
+               <div className="innerTicketContainerLeft">
+                  <div className={priority()}>.</div>
                 </div>
                 <div className="innerTicketContainer">
-                  <div className="subject">Subject: {prop.element.subject}</div>                
-                  <div className="location">Location: {prop.element.location}</div>
-                  <div className="contact">Contact: {prop.element.contact}</div>
+                  <div className="problem">Problem: {prop.elem.problem}</div>
+                  <div className="location">Location: {prop.elem.location}</div>
+                  <div className="contact">Contact: {prop.elem.point_of_contact}</div>
                 </div>
               </div>
     )
