a07a091a30f52940627ad8dd69f85927ff88fc8d
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-08 08:21:37 -0700

    Creates the campus report page

---
+++ src/campusReport/CampusReport.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./CampusReport.css"
+
+export default function CampusReport() {
+    return (
+        <div className="campusReportContainer">
+        <div className="campusReportHeader">Campus Report</div>
+        </div>
+    )
+}
