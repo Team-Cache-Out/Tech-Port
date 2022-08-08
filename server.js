2d4bb53a66e04e0ada883993fa4f82cf3a5c2902
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-08 12:25:46 -0700

    Updates the create ticket route to pull in the appropriate information.

--- server.js
+++ server.js
@@ -245,7 +245,7 @@ app.post("/tickets", async (req,res) => {
          /* Connecting to the database. */
         let client = await pool.connect();
         
-        const data = client.query("INSERT INTO tickets(close_date, open_date, problem, description, notes, point_of_contact, location, priority, status, assigned_tech, university_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)", [req.body.close_date, req.body.open_date, req.body.problem, req.body.description, req.body.notes, req.body.point_of_contact, req.body.location, req.body.priority, req.body.status, req.body.assigned_tech, req.body.university_id]);
+        const data = client.query("INSERT INTO tickets(problem, description, point_of_contact, location, priority, status, university_id) VALUES($1, $2, $3, $4, $5, $6, $7)", [req.body.problem, req.body.description, req.body.notes, req.body.point_of_contact, req.body.location, req.body.priority, req.body.status, req.body.university_id]);
         res.send(req.body);
 
         /* Releasing the client from the database. */
