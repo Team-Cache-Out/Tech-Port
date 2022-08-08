ffee512bcff6c3f59c1ea14614e70fce551ce7f8
Shatawni <shatawni.johnson@gmail.com>
2022-08-08 15:27:43 -0400

    Made changes to my ticket component.

--- src/App.js
+++ src/App.js
@@ -5,106 +5,183 @@ import SignUp from "./login-page/SignUp";
 import SignIn from "./login-page/SignIn";
 import AdminLandingPage from "./adminLandingPage/adminLandingPage";
 import TicketBoard from "./ticketBoard/ticketBoard";
-import CampusContext from './Context/CampusContext'
+import CampusContext from "./Context/CampusContext";
+import MyTickets from "./myTickets/MyTickets";
 
 function App() {
   const [modalIsOpen, setmodalIsOpen] = useState(true);
 
-  const { setHoustonOpenTickets, setHoustonWorkingTickets, setHoustonCompleteTickets } = useContext(CampusContext)
-  const { setArizonaOpenTickets, setArizonaWorkingTickets, setArizonaCompleteTickets } = useContext(CampusContext)
-  const { setOregonOpentTickets, setOregonWorkingTickets, setOregonCompleteTickets } = useContext(CampusContext)
-  const { setPepperdineOpenTickets, setPepperdineWorkingTickets, setPepperCompleteTickets } = useContext(CampusContext)
+  const {
+    setHoustonOpenTickets,
+    setHoustonWorkingTickets,
+    setHoustonCompleteTickets,
+  } = useContext(CampusContext);
+  const {
+    setArizonaOpenTickets,
+    setArizonaWorkingTickets,
+    setArizonaCompleteTickets,
+  } = useContext(CampusContext);
+  const {
+    setOregonOpentTickets,
+    setOregonWorkingTickets,
+    setOregonCompleteTickets,
+  } = useContext(CampusContext);
+  const {
+    setPepperdineOpenTickets,
+    setPepperdineWorkingTickets,
+    setPepperCompleteTickets,
+  } = useContext(CampusContext);
 
   // University of Houston Tickets
   useEffect(() => {
-    fetch(`https://worldwide-technical-foundation.herokuapp.com/campusTickets/1/open`)
-    .then(response => response.json())
-    .then(data => setHoustonOpenTickets(data))
+    fetch(
+      `https://worldwide-technical-foundation.herokuapp.com/campusTickets/1/open`
+    )
+      .then((response) => response.json())
+      .then((data) => setHoustonOpenTickets(data));
 
-    fetch(`https://worldwide-technical-foundation.herokuapp.com/campusTickets/1/working`)
-    .then(response => response.json())
-    .then(data => setHoustonWorkingTickets(data))
+    fetch(
+      `https://worldwide-technical-foundation.herokuapp.com/campusTickets/1/working`
+    )
+      .then((response) => response.json())
+      .then((data) => setHoustonWorkingTickets(data));
 
     fetch(`https://worldwide-technical-foundation.herokuapp.com/campusLog/1`)
-    .then(response => response.json())
-    .then(data => setHoustonCompleteTickets(data))
-  }, [setHoustonOpenTickets, setHoustonWorkingTickets, setHoustonCompleteTickets])
+      .then((response) => response.json())
+      .then((data) => setHoustonCompleteTickets(data));
+  }, [
+    setHoustonOpenTickets,
+    setHoustonWorkingTickets,
+    setHoustonCompleteTickets,
+  ]);
 
   // University of Arizona Tickets
   useEffect(() => {
-    fetch(`https://worldwide-technical-foundation.herokuapp.com/campusTickets/2/open`)
-    .then(response => response.json())
-    .then(data => setArizonaOpenTickets(data))
+    fetch(
+      `https://worldwide-technical-foundation.herokuapp.com/campusTickets/2/open`
+    )
+      .then((response) => response.json())
+      .then((data) => setArizonaOpenTickets(data));
 
-    fetch(`https://worldwide-technical-foundation.herokuapp.com/campusTickets/2/working`)
-    .then(response => response.json())
-    .then(data => setArizonaWorkingTickets(data))
+    fetch(
+      `https://worldwide-technical-foundation.herokuapp.com/campusTickets/2/working`
+    )
+      .then((response) => response.json())
+      .then((data) => setArizonaWorkingTickets(data));
 
     fetch(`https://worldwide-technical-foundation.herokuapp.com/campusLog/2`)
-    .then(response => response.json())
-    .then(data => setArizonaCompleteTickets(data))
-  }, [setArizonaOpenTickets, setArizonaWorkingTickets, setArizonaCompleteTickets])
-
+      .then((response) => response.json())
+      .then((data) => setArizonaCompleteTickets(data));
+  }, [
+    setArizonaOpenTickets,
+    setArizonaWorkingTickets,
+    setArizonaCompleteTickets,
+  ]);
 
   // University of Oregon Tickets
   useEffect(() => {
-    fetch(`https://worldwide-technical-foundation.herokuapp.com/campusTickets/3/open`)
-    .then(response => response.json())
-    .then(data => setOregonOpentTickets(data))
+    fetch(
+      `https://worldwide-technical-foundation.herokuapp.com/campusTickets/3/open`
+    )
+      .then((response) => response.json())
+      .then((data) => setOregonOpentTickets(data));
 
-    fetch(`https://worldwide-technical-foundation.herokuapp.com/campusTickets/3/working`)
-    .then(response => response.json())
-    .then(data => setOregonWorkingTickets(data))
+    fetch(
+      `https://worldwide-technical-foundation.herokuapp.com/campusTickets/3/working`
+    )
+      .then((response) => response.json())
+      .then((data) => setOregonWorkingTickets(data));
 
     fetch(`https://worldwide-technical-foundation.herokuapp.com/campusLog/3`)
-    .then(response => response.json())
-    .then(data => setOregonCompleteTickets(data))
-  }, [setOregonOpentTickets, setOregonWorkingTickets, setOregonCompleteTickets])
+      .then((response) => response.json())
+      .then((data) => setOregonCompleteTickets(data));
+  }, [
+    setOregonOpentTickets,
+    setOregonWorkingTickets,
+    setOregonCompleteTickets,
+  ]);
 
   // Pepperdine University Tickets
   useEffect(() => {
-    fetch(`https://worldwide-technical-foundation.herokuapp.com/campusTickets/4/open`)
-    .then(response => response.json())
-    .then(data => setPepperdineOpenTickets(data))
+    fetch(
+      `https://worldwide-technical-foundation.herokuapp.com/campusTickets/4/open`
+    )
+      .then((response) => response.json())
+      .then((data) => setPepperdineOpenTickets(data));
 
-    fetch(`https://worldwide-technical-foundation.herokuapp.com/campusTickets/4/working`)
-    .then(response => response.json())
-    .then(data => setPepperdineWorkingTickets(data))
+    fetch(
+      `https://worldwide-technical-foundation.herokuapp.com/campusTickets/4/working`
+    )
+      .then((response) => response.json())
+      .then((data) => setPepperdineWorkingTickets(data));
 
     fetch(`https://worldwide-technical-foundation.herokuapp.com/campusLog/4`)
-    .then(response => response.json())
-    .then(data => setPepperCompleteTickets(data))
-  }, [setPepperdineOpenTickets, setPepperdineWorkingTickets, setPepperCompleteTickets])
+      .then((response) => response.json())
+      .then((data) => setPepperCompleteTickets(data));
+  }, [
+    setPepperdineOpenTickets,
+    setPepperdineWorkingTickets,
+    setPepperCompleteTickets,
+  ]);
 
   return (
-  <Router>
-    <Routes>
-      <Route exact path='/' element={
-        <>
-        <LogIn></LogIn>
-        </>
-      }></Route>
-      <Route exact path='/signup' element={
-        <>
-        <LogIn></LogIn>
-        <SignUp modalIsOpen={modalIsOpen} setmodalIsOpen={setmodalIsOpen}></SignUp>
-        </>
-      }></Route>
-      <Route exact path='/signin' element={
-        <>
-        <SignIn modalIsOpen={modalIsOpen} setmodalIsOpen={setmodalIsOpen}></SignIn>
-        </>
-      }></Route>
-      <Route exact path='/admin' element={
-        <AdminLandingPage />
-      }></Route>
-      <Route exact path='/ticketBoard' element={
-        <>
-        <TicketBoard />
-        </>
-      }></Route>
-    </Routes>
-  </Router>   
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <LogIn></LogIn>
+            </>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/signup"
+          element={
+            <>
+              <LogIn></LogIn>
+              <SignUp
+                modalIsOpen={modalIsOpen}
+                setmodalIsOpen={setmodalIsOpen}
+              ></SignUp>
+            </>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/signin"
+          element={
+            <>
+              <SignIn
+                modalIsOpen={modalIsOpen}
+                setmodalIsOpen={setmodalIsOpen}
+              ></SignIn>
+            </>
+          }
+        ></Route>
+        <Route exact path="/admin" element={<AdminLandingPage />}></Route>
+        <Route
+          exact
+          path="/ticketBoard"
+          element={
+            <>
+              <TicketBoard />
+            </>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/myTickets"
+          element={
+            <>
+              <MyTickets />
+            </>
+          }
+        ></Route>
+      </Routes>
+    </Router>
   );
 }
 
