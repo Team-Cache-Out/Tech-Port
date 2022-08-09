31c51c4cff29621afc121954ada03a8edaf863f5
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-09 10:10:06 -0700

    Adds CSS for the assign divs and the selections divs

--- src/ticketBoard/singleTicketModal.js
+++ src/ticketBoard/singleTicketModal.js
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react'
 import CampusContext from '../Context/CampusContext'
+import SignInContext from '../Context/SignInContext'
 import './ticketModal.css'
 
 export default function SingleTicketModal({show}) {
 
     /* Destructuring the CampusContext object */
     const { setTicketModal, singleTicket, setSingleTicket } = useContext(CampusContext)
+
+    const { user } = useContext(SignInContext)
 
     /**
      * When the user clicks the close button, the ticket modal will close.
@@ -14,6 +17,10 @@ export default function SingleTicketModal({show}) {
         setTicketModal(false)
     }
 
+   /**
+    * It takes the value of the input field, adds it to an object, then sends it to the server via a
+    * PATCH request.
+    */
     const noteSubmit = () => {
         let data = {
             notes: addNote
@@ -35,6 +42,9 @@ export default function SingleTicketModal({show}) {
         })
     }
 
+   /**
+    * It takes the status of the ticket and updates it in the database.
+    */
     const statusUpdate = () => {
         let data = {
             status
@@ -56,8 +66,36 @@ export default function SingleTicketModal({show}) {
         })
     }
 
+    /* Setting the value of the variable addNote to an empty string. */
     let addNote = ''
-    let status = singleTicket.status.toUpperCase()
+    /* Setting the value of the variable status to the value of the singleTicket.status property. */
+    let status = singleTicket.status
+
+    const roleRights = () => {
+        
+        if(user.role === 'admin') {
+            return (
+                <div>
+                <form>
+                    <label>Assign Ticket:</label>
+                    <select defaultValue={null} >
+            
+                    </select>
+                </form>
+                <button className='SubmitNote-Button' id="SubmitTicket-Button" type='submit' onClick={statusUpdate}>Update</button>
+                </div>
+            )
+        } else {
+            return  (
+                <div>
+                <form>
+                    <label>Claim Ticket</label>
+                </form>
+                <button className='SubmitNote-Button' id="SubmitTicket-Button" type='submit' onClick={statusUpdate}>Claim</button>
+                </div>
+            )
+        }
+    }
 
     return (
         <>
@@ -85,20 +123,26 @@ export default function SingleTicketModal({show}) {
                             <label>Add Note</label>
                             <textarea rows = "10" cols = "80" name = "note" onChange={(e) => addNote = e.target.value} placeholder="Enter details here...">
                             </textarea>
-                            </form>
+                    </form>
                     <button className='SubmitNote-Button' id="SubmitTicket-Button" type='submit' onClick={noteSubmit}>Submit Note</button>
                     
-                    <div className='update'>
-                        <form> 
-                            <label>Update Status:</label>
-                            <select defaultValue={status} onChange={(e) => status = e.target.value}>
-                            <option value={null} disabled>Choose status</option>
-                            <option value="open">Open</option>
-                            <option value="working">Working</option>
-                            <option value="complete">Completed</option>
-                            </select>
-                        </form>
-                        <button className='SubmitNote-Button' id="SubmitTicket-Button" type='submit' onClick={statusUpdate}>Update</button>
+                    <div className='selections'>
+                        <div className='assign'>
+                            {roleRights()}
+                        </div>
+
+                        <div className='update'>
+                            <form> 
+                                <label>Update Status:</label>
+                                <select defaultValue={status} onChange={(e) => status = e.target.value}>
+                                <option value={null} disabled>Choose status</option>
+                                <option value="open">Open</option>
+                                <option value="working">Working</option>
+                                <option value="complete">Completed</option>
+                                </select>
+                            </form>
+                            <button className='SubmitNote-Button' id="SubmitTicket-Button" type='submit' onClick={statusUpdate}>Update</button>
+                        </div>
                     </div>
                 </div>
 
