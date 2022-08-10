d5e9050ea56636cf8382f3896fa69651e56c1250
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-10 09:07:44 -0700

    Imports the data needed for campus techs page and creates the initial map function

--- src/campusTechs/campusTechs.js
+++ src/campusTechs/campusTechs.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import CampusContext from "../Context/CampusContext";
+import SignInContext from "../Context/SignInContext";
 import "./campusTechs.css"
+import Tech from "./tech";
 
 export default function CampusTechs() {
+
+    const { HoustonTechs, ArizonaTechs, OregonTechs, PepperdineTechs } = useContext(CampusContext)
+
+    const { currentUni } = useContext(SignInContext)
+
+    const currentTechs = () => {
+        if(currentUni === 1) {
+            return HoustonTechs;
+        }
+        if(currentUni === 2) {
+            return ArizonaTechs;
+        }
+        if(currentUni === 3) {
+            return OregonTechs;
+        }
+        if(currentUni === 4) {
+            return PepperdineTechs;
+        }
+    }
+
     return (
         <div className="campusTechsContainer">
         <div className="campusTechsHeader">Campus Techs</div>
+        {/* {currentTechs().map((elem) => {
+            return (
+                <Tech />
+            )
+        })}*/}
         </div>
     )
 }
