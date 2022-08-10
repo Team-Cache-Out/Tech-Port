15481b61969a126dff6ab026dfa083834e3a1699
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-08-10 12:33:33 -0700

    Fixes the route for the create ticket page...hopefully

--- server.js
+++ server.js
@@ -241,9 +241,9 @@ app.post("/tickets", async (req,res) => {
     try {
          /* Connecting to the database. */
         let client = await pool.connect();
-        console.log(req.body)
-        // const data = client.query("INSERT INTO tickets (problem, description, point_of_contact, location, priority, status, university_id) VALUES($1, $2, $3, $4, $5, $6, $7)", [req.body.problem, req.body.description, req.body.notes, req.body.point_of_contact, req.body.location, req.body.priority, req.body.status, req.body.university_id]);
-        // res.send(data.rows[0]);
+        
+        const data = client.query("INSERT INTO tickets (problem, description, point_of_contact, location, priority, status, university_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", [req.body.problem, req.body.description, req.body.point_of_contact, req.body.location, req.body.priority, req.body.status, req.body.university_id]);
+        res.send(data.rows[0]);
 
         /* Releasing the client from the database. */
         client.release();
