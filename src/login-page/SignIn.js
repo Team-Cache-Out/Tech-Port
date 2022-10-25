aa4d9cabf4bf21fbe375a12f6b6fbe9d834ddbe6
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-10-25 08:34:26 -0700

    Added demo buttons for admins and techs

--- src/login-page/SignIn.js
+++ src/login-page/SignIn.js
@@ -18,22 +18,21 @@ function SignIn(props) {
 
   const navigate = useNavigate()
 
-  const submit = async (event) => {
+  const adminDemo = async (event) => {
     window.localStorage.clear();
-    event.preventDefault()
+    event.preventDefault();
     fetch(`https://worldwide-technical-foundation.herokuapp.com/users/login`, {
       method: 'POST',
       body: JSON.stringify({
-        'password': `${enteredPassword}`,
-        'email': `${enteredEmail}`
+        'password': `password`,
+        'email': `et.arcu@techport.com`
       }),
       headers: {
-        'Content-type' : 'application/json'
+        'Content-type': 'application/json'
       }
-    })   
-    .then (res => res.json())
-    .then (data => {
-      if(data.length !== 0) {
+    })
+      .then(res => res.json())
+      .then(data => {
         setUser(data[0])
         setCurrentUni(data[0].university_id)
         // window.alert("You have logged in!")
@@ -41,50 +40,105 @@ function SignIn(props) {
         if (data[0].role === 'admin') {
           navigate('/admin')
         }
-        else {
-          navigate('/ticketBoard')
-        }
-      } else {
-        window.alert("User not found!")
-        setUser(null)
       }
-    })     
+      )
   }
+
+  const techDemo = async (event) => {
+    window.localStorage.clear();
+    event.preventDefault()
+    fetch(`https://worldwide-technical-foundation.herokuapp.com/users/login`, {
+      method: 'POST',
+      body: JSON.stringify({
+        'password': `password`,
+        'email': `tempor@techport.com`
+      }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.length !== 0) {
+          setUser(data[0])
+          setCurrentUni(data[0].university_id)
+          // window.alert("You have logged in!")
+          setLoading(false)
+          if (data[0].role === 'tech') {
+            navigate('/ticketBoard')
+          }
+        }
+      })
+  }
+  // const submit = async (event) => {
+  //   window.localStorage.clear();
+  //   event.preventDefault()
+  //   fetch(`https://worldwide-technical-foundation.herokuapp.com/users/login`, {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       'password': `${enteredPassword}`,
+  //       'email': `${enteredEmail}`
+  //     }),
+  //     headers: {
+  //       'Content-type': 'application/json'
+  //     }
+  //   })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       if (data.length !== 0) {
+  //         setUser(data[0])
+  //         setCurrentUni(data[0].university_id)
+  //         // window.alert("You have logged in!")
+  //         setLoading(false)
+  //         if (data[0].role === 'admin') {
+  //           navigate('/admin')
+  //         }
+  //         else {
+  //           navigate('/ticketBoard')
+  //         }
+  //       } else {
+  //         window.alert("User not found!")
+  //         setUser(null)
+  //       }
+  //     })
+  // }
 
   const handleSignUp = () => {
     props.setmodalIsOpen(true);
   }
-useEffect(()=>{
-  if (user != null) {
-    if (user.role === "tech") {
-      navigate("/ticketboard");
+  useEffect(() => {
+    if (user != null) {
+      if (user.role === "tech") {
+        navigate("/ticketboard");
+      }
+      else {
+        navigate("/admin");
+      }
     }
-    else {
-      navigate("/admin");
-    }
-    }
-})
+  })
   return (
     <div className='SignIn-Container'>
       <h2 className='signIn-Header'>SIGN IN</h2>
       <form className='SignIn-Form' id='SignIn-Form'>
-          <label>EMAIL</label> <br/>
-          <input placeholder='example@email.com' className='Email-Input' id='sigIn-email'onChange={handleEmailChange} value={enteredEmail} >
-          </input>
-          <br/>
-          <br/>
-          <label>PASSWORD</label>
-          <br></br>
-          <input placeholder='Password...' className='Password-Input'  type="password" id='signIn-password' onChange={handlePassChange} value={enteredPassword} > 
-          </input>
-          <br/>
-          <br></br>
-          <button className='SignIn-Button' id="SignIn-Button" form="SignIn-Form" type='submit' onClick={submit}>Sign In
-          </button>
-          <br/>
-          <p className='NoAccount-Info'>Don't have an account? Click <Link to="/signup" onClick={handleSignUp} className="SignUp-Link">Here</Link> </p>
-      </form>      
-    </div>    
+        <label>EMAIL</label> <br />
+        <input placeholder='example@email.com' className='Email-Input' id='sigIn-email' onChange={handleEmailChange} value={enteredEmail} disabled>
+        </input>
+        <br />
+        <br />
+        <label>PASSWORD</label>
+        <br></br>
+        <input placeholder='Password...' className='Password-Input' type="password" id='signIn-password' onChange={handlePassChange} value={enteredPassword} disabled>
+        </input>
+        <br />
+        <br></br>
+        <button className='SignIn-Button' id="SignIn-Button" form="SignIn-Form" type='submit' onClick={adminDemo}>Admin Demo Login
+        </button>
+        <button className='SignIn-Button' id="SignIn-Button" form="SignIn-Form" type='submit' onClick={techDemo}>Tech Demo Login
+        </button>
+        <br />
+        <p className='NoAccount-Info'>Don't have an account? Click <Link to="/signup" onClick={handleSignUp} className="SignUp-Link">Here</Link> </p>
+      </form>
+    </div>
   )
 
 
